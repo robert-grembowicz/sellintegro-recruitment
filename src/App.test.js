f87949eb404893776245144f007eaf04e5a09fa6
Robert Grembowicz <robert.grembowicz@gmail.com>
2023-12-18 16:51:39 +0100

    adding tests to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App testing', () => {
+
+  const texts = [
+    'learn react',
+    'edit',
+    'src/App.js',
+    'learn React'
+  ]
+
+  it('should renders all texts', () => {
+    render(<App />);
+    texts.forEach(item => {
+      const text = screen.getByText(new RegExp(item, 'i'));
+      expect(text).toBeVisible();
+    })
+  });
+
+  it('should have clickable link', () => {
+    render(<App />);
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('href', 'https://reactjs.org')
+  });
+
+  it('link should targetting to new window', () => {
+    render(<App />)
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('target', '_blank')
+  })
+})
